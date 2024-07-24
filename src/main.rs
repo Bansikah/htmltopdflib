9ca8085722d53332b673699c8890b4ef712bf907
Bansikah <88171256+Bansikah@users.noreply.github.com>
2024-07-24 10:16:53 +0100

    remove comments

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,20 @@ use std::fs;
 use std::path::Path;
 use anyhow::Context;
 use anyhow::Result;
-use html_to_pdf_lib::html_to_pdf; // Import your library function
+use html_to_pdf_lib::html_to_pdf; 
 
 fn main() -> Result<()> {
-    // Ensure the correct number of arguments
+   
     if env::args().len() != 2 {
         eprintln!("Usage: {} <input_html_file>", env::args().next().unwrap());
         std::process::exit(1);
     }
 
-    // Get the input file path from the command line arguments
+    
     let input_path = env::args().nth(1).expect("No input file provided");
     let output_path = Path::new("output.pdf");
 
-    // Read the HTML file content
+    
     let html = fs::read_to_string(&input_path)
         .context("Failed to read input file")?;
 
